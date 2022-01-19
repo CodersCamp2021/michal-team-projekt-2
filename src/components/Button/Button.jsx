5cc71a1c995c367b6a27009f9ba048db71fe6408
Michal Miszczyszyn <michal@mmiszy.pl>
2022-01-19 01:31:40 +0100

    Code Review

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,10 @@
 import { Link } from 'react-router-dom';
 import styles from './Button.module.scss';
 
-export const Button = (props) => {
+export const Button = ({ description, path }) => {
   return (
-    <Link className={styles.button} to={props.path}>
-      {props.description}
+    <Link className={styles.button} to={path}>
+      {description}
     </Link>
   );
 };
