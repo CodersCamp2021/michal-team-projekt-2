44b6cab09e7186fca3ef6f15dd0a2a45b9eb2e03
Michal Miszczyszyn <michal@mmiszy.pl>
2022-01-19 01:08:59 +0100

    chore: Migrate to scss modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss';
 
 export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles.app}>
+      <header className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
         <p>
           Edit <code>src/App.jsx</code> and save to reload.
         </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        <a className={styles.appLink} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
