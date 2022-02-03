0f7e0fd7bb35a677a4bd5ff595a849b5d276bc10
Weronika Burzyńska <40180443+Weroniika@users.noreply.github.com>
2022-02-03 13:42:41 +0100

    Add AuthContext (#40)
    * feat: add AuthContext
    
    * review fixes
    
    * review fixes
    
    * review fixes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,10 @@
 import { LoginForm } from '../components/Login Form/LoginForm';
+import { useAuth } from '../context/authContext';
 
 export function Login() {
-  return <LoginForm />;
+  const { signIn } = useAuth();
+  const onSubmit = (data) => {
+    signIn(data);
+  };
+  return <LoginForm onSubmit={onSubmit} />;
 }
