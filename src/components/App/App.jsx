ce02b06fbf6991b0051c4f0640225e2e147ffeba
Mateusz Binięda <93389452+Arssin@users.noreply.github.com>
2022-02-06 17:21:05 +0100

    Add object form (#53)
    * conflict resolve
    
    * Add object Layout
    
    * Key hide
    
    * Add object form
    
    * Delete no needed function
    
    * Review fixes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,6 +5,7 @@ import { Register } from '../../pages/Register';
 import { SingleOffer } from '../../pages/SingleOffer/SingleOffer';
 import { Header } from '../Header/Header';
 import { AuthProvider } from '../../context/authContext';
+import { AddObject } from '../../pages/AddObject';
 import { RemindPassword } from '../RemindPasswordForm/RemindPassword';
 
 export const App = () => {
@@ -17,6 +18,7 @@ export const App = () => {
           <Route exact path="/register" element={<Register />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/single-offer" element={<SingleOffer />} />
+          <Route exact path="/add-object" element={<AddObject />} />
           <Route exact path="/remind-password" element={<RemindPassword />} />
         </Routes>
       </Router>
--- src/components/SectionWithUnderlineTitle/SectionWithUnderlineTitle.jsx
+++ src/components/SectionWithUnderlineTitle/SectionWithUnderlineTitle.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styles from './SectionWithUnderlineTitle.module.scss';
 
 export const SectionWithUnderlineTitle = ({ title, id, children }) => {
