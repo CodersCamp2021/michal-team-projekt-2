baea92d0f963517f5e3c53babcabe2872e53e8bd
Kamil Kantorski <93733349+KamiKant@users.noreply.github.com>
2022-02-07 23:01:02 +0100

    small changes in filtres component

--- src/pages/OffersPage/OffersPage.jsx
+++ src/pages/OffersPage/OffersPage.jsx
@@ -1,15 +1,19 @@
 import { SearchForm } from '../../components/SearchForm/SearchForm';
 import { SearchFilters } from '../../components/SearchFilters/SearchFilters';
-import { ObjectsList } from '../../components/ObjectsList/ObjectsList';
+// import { ObjectsList } from '../../components/ObjectsList/ObjectsList';
+// import { Object } from '../../components/ObjectsList/Object/Object';
 import styles from './OffersPage.module.scss';
 
 export function OffersPage() {
   return (
     <div className={styles.wrapper}>
-      <SearchForm />
-      <div>
-        <SearchFilters />
-        {/* <ObjectsList /> */}
+      <SearchForm saveData={() => {}} />
+      <div className={styles.container}>
+        <div className={styles.filters}>
+          <SearchFilters />
+        </div>
+        <div className={styles.objectsList}>test</div>
+        {/* <ObjectList /> */}
       </div>
     </div>
   );
