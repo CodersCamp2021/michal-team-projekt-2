10a2f970d7ad69b4dd7a3f3491ced672e48aaf7d
Weronika Burzyńska <40180443+Weroniika@users.noreply.github.com>
2022-02-09 16:10:09 +0100

    fix: adjust layout with footer (#81)

--- src/layouts/LayoutWithHeaderAndFooter.jsx
+++ src/layouts/LayoutWithHeaderAndFooter.jsx
@@ -1,12 +1,13 @@
 import { Footer } from '../components/Footer/Footer';
 import { Header } from '../components/Header/Header';
+import styles from './Layout.module.scss';
 
 export const LayoutWithHeaderAndFooter = ({ children }) => {
   return (
-    <>
+    <div className={styles.container}>
       <Header />
       {children}
       <Footer />
-    </>
+    </div>
   );
 };
