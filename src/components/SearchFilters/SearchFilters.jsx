a3d256e80f545eaf2ac0ca18339a4a172221ca99
KamiKant <93733349+KamiKant@users.noreply.github.com>
2022-02-10 17:12:07 +0100

    Add offers-page UI (#80)
    * add offers UI
    
    * fix offers ui
    
    * Update Filters
    
    Co-authored-by: Karol Wolski <karolwolski04@gmail.com>
    
    * Update Filters
    
    Co-authored-by: Karol Wolski <karolwolski04@gmail.com>
    
    * addprice-filter logic
    
    * Update Offers
    
    Co-authored-by: Weronika Burzyńska <40180443+Weroniika@users.noreply.github.com>
    
    * Update Offers
    
    Co-authored-by: Weronika Burzyńska <40180443+Weroniika@users.noreply.github.com>
    
    * Update Offers
    
    Co-authored-by: Weronika Burzyńska <40180443+Weroniika@users.noreply.github.com>
    
    * minor fix offers-page
    
    * Update SearchFilters
    
    Co-authored-by: Weronika Burzyńska <40180443+Weroniika@users.noreply.github.com>
    
    * small update offers-page
    
    Co-authored-by: Karol Wolski <karolwolski04@gmail.com>
    Co-authored-by: Weronika Burzyńska <40180443+Weroniika@users.noreply.github.com>
    Co-authored-by: Krystian Gaczyński <30621967+krygacz@users.noreply.github.com>

---
+++ src/components/SearchFilters/SearchFilters.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+import { Filters } from './Filters/Filters';
+import styles from './SearchFilters.module.scss';
+
+export function SearchFilters(props) {
+  const [open, setOpen] = useState(false);
+  const [disabled, setDisabled] = useState(false);
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResizeWindow = () => setWidth(window.innerWidth);
+    window.addEventListener('resize', handleResizeWindow);
+    if (width >= 952) {
+      setOpen(true);
+      setDisabled(true);
+    } else {
+      setDisabled(false);
+    }
+    return () => {
+      window.removeEventListener('resize', handleResizeWindow);
+    };
+  }, [width]);
+
+  const onClick = (e) => {
+    setOpen(!open);
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <button className={styles.header} onClick={onClick} disabled={disabled}>
+        <p className={styles.headerName}>Filtry</p>
+      </button>
+      {open && <Filters {...props} />}
+    </div>
+  );
+}
---
+++ src/pages/Offers/Offers.jsx
@@ -0,0 +1,98 @@
+import { useState, useEffect } from 'react';
+import { SearchForm } from '../../components/SearchForm/SearchForm';
+import { SearchFilters } from '../../components/SearchFilters/SearchFilters';
+import { ObjectsList } from '../../components/ObjectsList/ObjectsList';
+import styles from './Offers.module.scss';
+
+const data = {
+  city: 'Warszawa',
+  numOfObjects: 2,
+  objects: [
+    {
+      id: 1,
+      title: 'Apartament z widokiem na morze',
+      description: `
+    Velit culpa ipsum irure duis incididunt duis occaecat consequat nulla excepteur duis non nisi ad. Elit
+    Lorem reprehenderit ullamco laborum ex ex amet laborum tempor.`,
+      localisation: {
+        address: 'Łazienkowska 3, Warsaw, Poland',
+        latitude: 52.22136,
+        longitude: 21.04067,
+      },
+      price: 169,
+      oldPrice: 239,
+      image:
+        'https://images.unsplash.com/photo-1631048501851-4aa85ffc3be8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    },
+    {
+      id: 2,
+      title: 'Apartament z widokiem na morze',
+      description: `
+    Velit culpa ipsum irure duis incididunt duis occaecat consequat nulla excepteur duis non nisi ad. Elit
+    Lorem reprehenderit ullamco laborum ex ex amet laborum tempor.`,
+      localisation: {
+        address: 'Łazienkowska 3, Warsaw, Poland',
+        latitude: 52.22136,
+        longitude: 21.04067,
+      },
+      price: 179,
+      image:
+        'https://images.unsplash.com/photo-1631048501851-4aa85ffc3be8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    },
+  ],
+};
+
+export function Offers() {
+  const [minPrice, setMinPrice] = useState(10);
+  const [maxPrice, setMaxPrice] = useState(400);
+  const [newObject, setNewObject] = useState({
+    city: data.city,
+    numOfObjects: data.objects.length,
+    objects: data.objects,
+  });
+
+  const handleValue = (e) => {
+    if (e.target.name === 'maxPrice') {
+      setMaxPrice(e.target.value);
+    } else if (e.target.name === 'minPrice') {
+      setMinPrice(e.target.value);
+    }
+  };
+
+  const handleCheckbox = () => {};
+
+  useEffect(() => {
+    const filterPrice = (data) => {
+      const newData = data.objects.filter((x) => x.price > parseInt(minPrice, 10) && x.price <= parseInt(maxPrice, 10));
+      const createObject = {
+        city: data.city,
+        numOfObjects: newData.length,
+        objects: newData,
+      };
+      setNewObject(createObject);
+    };
+    filterPrice(data);
+  }, [minPrice, maxPrice]);
+
+  const saveData = (data) => {
+    console.log(data);
+  };
+  return (
+    <div className={styles.wrapper}>
+      <SearchForm saveData={saveData} />
+      <div className={styles.container}>
+        <div className={styles.filters}>
+          <SearchFilters
+            handleValue={handleValue}
+            handleCheckbox={handleCheckbox}
+            minValue={minPrice}
+            maxValue={maxPrice}
+          />
+        </div>
+        <div className={styles.objectsList}>
+          <ObjectsList objects={newObject} />
+        </div>
+      </div>
+    </div>
+  );
+}
