1c906999361065ec2aa1d5b1d29c9cc6f1e32edf
Karol Wolski <karolwolski04@gmail.com>
2022-02-10 16:29:28 +0100

    fix(Object): change heading to link (#85)

--- src/components/ObjectsList/Object/Object.jsx
+++ src/components/ObjectsList/Object/Object.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+import { convertToSlug } from '../../../helpers/convertToSlug';
 import styles from './Object.module.scss';
 
 export const Object = ({ object }) => {
@@ -8,10 +10,13 @@ export const Object = ({ object }) => {
       className={oldPrice ? `${styles.object} ${styles.objectAccent}` : styles.object}
       data-bargain={oldPrice && `Okazja dnia`}
     >
+      <Link to={convertToSlug(title)} className={styles.objectLink}></Link>
       <img src={image} alt="" className={styles.objectImage} />
       <div className={styles.objectContainer}>
         <div>
-          <h3 className={styles.objectTitle}>{title}</h3>
+          <h3 to={convertToSlug(title)} className={styles.objectTitle}>
+            {title}
+          </h3>
           <p className={styles.objectDesc}>{description}</p>
         </div>
         <p className={oldPrice ? `${styles.objectPrice} ${styles.objectPriceNew}` : styles.objectPrice}>
