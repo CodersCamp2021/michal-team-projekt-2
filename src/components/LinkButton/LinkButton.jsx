76d6e73e47aadcf3e27d07c0202a9bd81f98cedb
weroniika <vburzynska@outlook.com>
2022-02-21 21:41:56 +0100

    test: Header, small refactor LinkButton

--- src/components/LinkButton/LinkButton.jsx
+++ src/components/LinkButton/LinkButton.jsx
@@ -1,9 +1,14 @@
 import { Link } from 'react-router-dom';
 import styles from '../Button/Button.module.scss';
 
-export const LinkButton = ({ children, path, onClick, upper }) => {
+export const LinkButton = ({ children, path, onClick, upper, ariaLabel }) => {
   return (
-    <Link to={path} className={`${upper && styles.buttonUpper} ${styles.button}`} onClick={onClick}>
+    <Link
+      to={path}
+      className={`${upper ? styles.buttonUpper : ''} ${styles.button}`}
+      onClick={onClick}
+      aria-label={ariaLabel}
+    >
       {children}
     </Link>
   );
