8e189d961722a667cc301944dfc94de5aa49d9d5
Arssin <mateusz.binieda@onet.pl>
2022-02-27 20:40:58 +0100

    Delete comment

--- src/components/DetailsSummary/DetailsSummary.test.jsx
+++ src/components/DetailsSummary/DetailsSummary.test.jsx
@@ -1,15 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { DetailsSummary } from './DetailsSummary';
 
-// const data = {
-//   status: 'zarezerwowany',
-//   phone: '+48 500 100 200',
-//   address: 'Łazienkowska 3, Warszawa, Polska',
-//   title: 'Apartament z widokiem na morze',
-//   image:
-//     'https://images.unsplash.com/photo-1631048501851-4aa85ffc3be8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-// };
-
 describe('DetailsSummary', () => {
   it('should render component with correct values', () => {
     render(
