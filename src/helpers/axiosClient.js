60aa5c1970670c4a9f165100f815ab4baceb7292
weroniika <vburzynska@outlook.com>
2022-03-17 17:44:46 +0100

    fix: axiosClient

--- src/helpers/axiosClient.js
+++ src/helpers/axiosClient.js
@@ -5,6 +5,33 @@ export const axiosClient = axios.create({
   withCredentials: true,
 });
 
+export const noInterceptAxiosClient = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+});
+
+const responseSuccessHandler = async (response) => {
+  return response;
+};
+
+const responseErrorHandler = async (error) => {
+  const config = error.config;
+  const code = error.response.status;
+  if (code === 401 && config.url === '/auth/token') {
+    return Promise.reject(error);
+  }
+  if (code === 401 && !config._retry) {
+    config._retry = true;
+    const res = await noInterceptAxiosClient.post('/auth/token', {
+      refreshToken: localStorage.getItem('refreshToken'),
+    });
+    if (res) {
+      const { token } = res.data;
+      localStorage.setItem('token', token);
+    }
+    return Promise.reject(error);
+  }
+};
+
 axiosClient.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
@@ -19,27 +46,6 @@ axiosClient.interceptors.request.use(
 );
 
 axiosClient.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async (error) => {
-    const config = error.config;
-    if (error.response.status === 401 && config.url === '/auth/token') {
-      return Promise.reject(error);
-    }
-    if (error.response.status === 401 && !config._retry) {
-      config._retry = true;
-      try {
-        const res = await axiosClient.post('/auth/token', {
-          refreshToken: localStorage.getItem('refreshToken'),
-        });
-        const { token } = res.data;
-        localStorage.setItem('token', token);
-        window.location.reload();
-        return config;
-      } catch (error) {
-        return Promise.reject(error);
-      }
-    }
-  },
+  (response) => responseSuccessHandler(response),
+  async (error) => responseErrorHandler(error),
 );
