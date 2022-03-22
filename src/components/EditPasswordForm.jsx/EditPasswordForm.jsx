cd058756aaccb18c592f1a9313834e42e19c6f04
Arssin <mateusz.binieda@onet.pl>
2022-03-22 23:25:04 +0100

    Integrate password change

--- src/components/EditPasswordForm.jsx/EditPasswordForm.jsx
+++ src/components/EditPasswordForm.jsx/EditPasswordForm.jsx
@@ -1,26 +1,41 @@
 import { useForm } from 'react-hook-form';
+import { useState } from 'react';
 import { ButtonForm } from '../ButtonForm/ButtonForm';
 import { ErrorMessage } from '../ErrorMessage/ErrorMessage';
 import { passwordValidation } from '../../helpers/validators';
 import styles from '../../styles/forms.module.scss';
+import { userService } from '../../services/user';
+import { dirtyValues } from '../../helpers/dirtyValues';
 
-export function EditPasswordForm({ onSubmit }) {
+export function EditPasswordForm({ userData }) {
+  const [message, setMessage] = useState('');
   const {
     register,
     handleSubmit,
-    formState: { errors, isValid, isDirty },
+    formState: { errors, isValid, isDirty, dirtyFields },
     getValues,
   } = useForm({
     mode: 'onChange',
+    defaultValues: {
+      ...userData,
+    },
   });
+
+  const onSubmit = async (data) => {
+    const updatedFields = dirtyValues(dirtyFields, data);
+    const updatedData = await userService.updateMe(updatedFields);
+    if (updatedData) {
+      setMessage('Your password has been successfully updated');
+    }
+  };
 
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
         <label className={styles.label}>
           <span className={styles.labelName}>Stare hasło: </span>
-          <input className={styles.input} type="password" {...register('oldPassword', { ...passwordValidation })} />
-          {errors.oldPassword && <ErrorMessage message={errors.oldPassword.message} />}
+          <input className={styles.input} type="password" {...register('password', { ...passwordValidation })} />
+          {errors.password && <ErrorMessage message={errors.password.message} />}
         </label>
         <label className={styles.label}>
           <span className={styles.labelName}>Hasło: </span>
@@ -32,13 +47,14 @@ export function EditPasswordForm({ onSubmit }) {
           <input
             className={styles.input}
             type="password"
-            {...register('repassword', { validate: (value) => value === getValues('password') })}
+            {...register('password', { validate: (value) => value === getValues('password') })}
           />
-          {errors.repassword && <ErrorMessage message="Hasła muszą być identyczne" />}
+          {errors.password && <ErrorMessage message="Hasła muszą być identyczne" />}
         </label>
         <div className={styles.buttonWrapper}>
           <ButtonForm name="Zapisz zmiany" disabled={!isValid || !isDirty} />
         </div>
+        {message && <span className={styles.success}>{message}</span>}
       </form>
     </div>
   );
--- src/pages/ChangePassword/ChangePassword.jsx
+++ src/pages/ChangePassword/ChangePassword.jsx
@@ -1,12 +1,25 @@
+import { useEffect, useState } from 'react';
 import { EditPasswordForm } from '../../components/EditPasswordForm.jsx/EditPasswordForm';
 import { LayoutUserAccount } from '../../layouts/LayoutUserAccount';
+import { userService } from '../../services/user';
+import { Loading } from '../../components/Loading/Loading';
 
 export const ChangePassword = () => {
-  const onSubmit = () => console.log('send data');
-  return (
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    userService.getMe().then((data) => {
+      const { password } = data.data;
+      setUserData({ password });
+    });
+  }, []);
+
+  return userData ? (
     <LayoutUserAccount>
       <h1>Zmień hasło:</h1>
-      <EditPasswordForm onSubmit={onSubmit} />
+      <EditPasswordForm userData={userData} />
     </LayoutUserAccount>
+  ) : (
+    <Loading />
   );
 };
