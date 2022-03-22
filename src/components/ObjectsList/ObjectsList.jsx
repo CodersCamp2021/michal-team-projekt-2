dffe2377ee062e885741dd63f864ca45cea41900
Krystian Gaczyński <30621967+krygacz@users.noreply.github.com>
2022-03-22 11:17:23 +0100

    Integrate single offer view

--- src/components/ObjectsList/ObjectsList.jsx
+++ src/components/ObjectsList/ObjectsList.jsx
@@ -1,5 +1,6 @@
 import { polishPlurals } from 'polish-plurals';
 import { useSearchContext } from '../../context/searchContext';
+import { Loading } from '../Loading/Loading';
 import { Object } from './Object/Object';
 import styles from './ObjectsList.module.scss';
 
@@ -9,15 +10,19 @@ export const ObjectsList = ({ objects }) => {
   } = useSearchContext();
   return (
     <>
-      <div className={styles.objectsList}>
-        <h2 className={styles.objectsListTitle}>
-          <span>{localisation}: </span>
-          znaleziono {objects.length} {polishPlurals('obiekt', 'obiekty', 'obiektów', objects.length)}
-        </h2>
-        {objects.map((object) => (
-          <Object key={object._id} object={object} />
-        ))}
-      </div>
+      {!objects ? (
+        <Loading />
+      ) : (
+        <div className={styles.objectsList}>
+          <h2 className={styles.objectsListTitle}>
+            <span>{localisation}: </span>
+            znaleziono {objects.length} {polishPlurals('obiekt', 'obiekty', 'obiektów', objects.length)}
+          </h2>
+          {objects.map((object) => (
+            <Object key={object._id} object={object} />
+          ))}
+        </div>
+      )}
     </>
   );
 };
